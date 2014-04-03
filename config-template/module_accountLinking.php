779b6ff49512ec431cc23f545526e1e91056b6ba
Sixto Martin <smartin@yaco.es>
2014-04-03 19:01:23 +0200

    Auth LoAs should be defined at the config file

--- config-template/module_accountLinking.php
+++ config-template/module_accountLinking.php
@@ -9,14 +9,17 @@ $config = array(
     'disable-non-compliance' => false,
 
     // Default LoA value to be used for idps and sps that are not
-    // defined in the LoAs var.
+    // defined in the LoAs var or in the auth source.
     'default-LoAs' => array (
         'sp' => 2,
         'idp' => 1,
+        'auth' => 1,
     ),
 
     // Array that contains a list of 'idps' and 'sps', each
-    // entityId is associated with the required LoA  
+    // entityId is associated with the required LoA. And also
+    // define the LoAs of the auth sources that may be configured
+    // at the config/authsources.php
     'LoAs' => array (
         'idps' => array (
             'http://idp1.example.com' => 3,
@@ -24,8 +27,13 @@ $config = array(
         ),
         'sps' => array (
             'http://sp1.example.com' => 2,
-            'http://sp2.example.com' => 1,
+            'http://sp2.example.com' => 1
         ),
+        'auths' => array (
+            'example-admin' => 3,
+            'google' => 1,
+            'facebook' => 1
+        )
     ),
 );
 
